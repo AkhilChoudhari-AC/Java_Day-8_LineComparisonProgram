52cca631fbb3c03e339edf4662cae8f5877bbec8
Akhil-Choudhari <akhil.choudhari.ac@gmail.com>
2022-07-04 20:09:07 +0530

    STARTED: Welcome to line Comparison Program

---
+++ LineComparison.java
@@ -0,0 +1,10 @@
+package LineComparisonProgram;
+
+public class LineComparison {
+    public static void lineComparision() {
+        System.out.println("Welcome To Line Comparison Program");
+    }
+    public static void main(String[] args) {
+        lineComparision();
+    }
+}
