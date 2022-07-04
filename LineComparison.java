6bf3b1488e85df8e02e6d09c3d03ec2ae6bbb0f3
Akhil-Choudhari <akhil.choudhari.ac@gmail.com>
2022-07-04 20:14:17 +0530

    ADDED: Input coordinates using Cartesian System.

--- LineComparison.java
+++ LineComparison.java
@@ -1,10 +1,26 @@
 package LineComparisonProgram;
 
+import java.util.Scanner;
+
 public class LineComparison {
     public static void lineComparision() {
-        System.out.println("Welcome To Line Comparison Program");
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the value of x1: ");
+        int x1 = sc.nextInt();
+        System.out.println("Enter the value of x2: ");
+        int x2 = sc.nextInt();
+        System.out.println("Enter the value of y1: ");
+        int y1 = sc.nextInt();
+        System.out.println("Enter the value of y2: ");
+        int y2 = sc.nextInt();
+
+        double length1 = Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+        System.out.println("Length of the line is: " + length1);
+        sc.close();
     }
     public static void main(String[] args) {
+
         lineComparision();
     }
 }
